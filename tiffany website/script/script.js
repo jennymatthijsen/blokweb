5ea0c484ac094d7256f58b04e3123d990b8a0b26
jennymatthijsen <74053453+jennymatthijsen@users.noreply.github.com>
2020-12-10 12:22:40 +0100

    Add files via upload

---
+++ tiffany website/script/script.js
@@ -0,0 +1,9 @@
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
