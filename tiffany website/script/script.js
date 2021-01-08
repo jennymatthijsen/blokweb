68bbc6bae44db787182ef7cb4d2c7a2f72fae123
jennymatthijsen <74053453+jennymatthijsen@users.noreply.github.com>
2021-01-08 15:49:27 +0100

    Add files via upload

--- tiffany website/script/script.js
+++ tiffany website/script/script.js
@@ -4,7 +4,11 @@ menu.addEventListener("click", hamburger);
 
 function hamburger(event) {
   deNav = event.target.parentNode;
-  deNav.classList.toggle("hamburgermenu");
+  deNav.classList.toggle("menuUitklappen");
 }
 
 // een deel van deze JS voor het hamburgermenu is afkomstig van oefening 3 van Media Queries -> https://codepen.io/jennymatthijsen/pen/zYKrYyw
+
+// de document.querySelector roept de nav button aan en zet daar door de addEventListener, die de funcitie aanroept een class op
+// met die class heb ik in css het hamburgermenu kunnen maken
+// de functie wordt uitgevoerd wanneer je op het menu klikt. Vandaar de "click"
